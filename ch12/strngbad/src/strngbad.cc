e51d0da2702301f7b9587ccdde7d504be4cff3c1
김동진 <kdj6724@naver.com>
2017-10-23 13:31:52 +0900

    [ADD] default constructor example

---
+++ ch12/strngbad/src/strngbad.cc
@@ -0,0 +1,53 @@
+// strngbad.cc
+#include <cstring>
+#include "strngbad.h"
+
+int StringBad::num_strings = 0;
+
+StringBad::StringBad(const char* s) {
+  len = std::strlen(s);
+  str = new char[len + 1];
+  std::strcpy(str,s);
+  num_strings++;
+  std::cout << num_strings << ": \"" << str << "\" object created\n";
+}
+
+StringBad::StringBad() {
+  len = 4;
+  str = new char[4];
+  std::strcpy(str, "C++");
+  num_strings++;
+  std::cout << num_strings << ": \"" << str << "\" default object created\n";
+}
+
+StringBad::StringBad(const StringBad& st) {
+  num_strings++;
+  len = st.len;
+  str = new char [len +1];
+  std::strcpy(str, st.str);
+  std::cout << num_strings << ": \"" << str
+      << "\" object created\n";
+}
+
+StringBad::~StringBad() {
+  std::cout << "\"" << str << "\" objectdeleted, ";
+  --num_strings;
+  std::cout << num_strings << " left\n";
+  delete[] str;
+}
+
+StringBad& StringBad::operator=(const StringBad& st) {
+  if (this == &st)
+    return *this;
+  delete [] str;
+  len = st.len;
+  str = new char [len + 1];
+  std::strcpy(str, st.str);
+  return *this;
+}
+
+std::ostream& operator<<(std::ostream& os, const StringBad& st) {
+  os << st.str;
+  return os;
+}
+
---
+++ ch12/strngbad/src/strngbad.h
@@ -0,0 +1,18 @@
+// strngbad.h
+#include <iostream>
+#ifndef STRNGBAD_H_
+#define STRNGBAD_H_
+class StringBad {
+private:
+  char* str;
+  int len;
+  static int num_strings;
+public:
+  StringBad(const char* s);
+  StringBad();
+  StringBad(const StringBad& st);
+  ~StringBad();
+  StringBad & operator=(const StringBad &);
+  friend std::ostream& operator<<(std::ostream& os, const StringBad& st);
+};
+#endif  // STRNGBAD_H_
